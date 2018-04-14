1202572862242bbe5fb402ff56036fc0bc1e120f
Jessica Linares <jessicaalvarado1777@gmail.com>
2018-04-13 22:58:56 -0600

    Enlace

---
+++ src/pruebaexamenmetodologia/PruebaExamenMetodologia.java
@@ -0,0 +1,21 @@
+
+package pruebaexamenmetodologia;
+
+import javax.swing.JOptionPane;
+
+/**
+ *Nombre de la clase: PruebaExamenMetologia
+ * Fecha: 13-04-2018
+ * Version: 1.0
+ * CopyRight: Jessica Menjivar
+ * @author Jessica Linares
+ */
+public class PruebaExamenMetodologia {
+
+
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "HOLA...", "Saludo",1);
+    }
+    
+}
